03dd4591fee21c3379547db8e5e26fd8a37cc5d2
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-09-18 18:06:51 -0400

    Almost done?!

--- Source/TransformativeFramework/Utilities/TFColorKeyPair.cs
+++ Source/TransformativeFramework/Utilities/TFColorKeyPair.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 using Verse;
 
 #pragma warning disable IDE1006 // Naming Styles
