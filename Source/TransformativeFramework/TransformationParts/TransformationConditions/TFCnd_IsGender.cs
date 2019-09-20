489d8c2cf44526ef8ca21b3874b41395fba41f0b
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-09-19 20:23:15 -0400

    Create TFCnd_IsGender

---
+++ Source/TransformativeFramework/TransformationParts/TransformationConditions/TFCnd_IsGender.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.TFs
+{
+   public class TFCnd_IsGender : TransformationCondition
+   {
+      public Gender gender;
+
+      // Returns true if pawn is the specified gender.
+      protected override bool CheckPartWorker(Pawn pawn, object cause)
+      {
+         return pawn.gender == gender;
+      }
+   }
+}
