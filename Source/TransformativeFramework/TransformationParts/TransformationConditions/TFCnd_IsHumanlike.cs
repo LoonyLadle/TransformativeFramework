473312f8a3b45d925372fa5f9fe921ae3c439bab
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-09-19 22:03:54 -0400

    Create TFCnd_IsHumanlike

---
+++ Source/TransformativeFramework/TransformationParts/TransformationConditions/TFCnd_IsHumanlike.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.TFs
+{
+   public class TFCnd_IsHumanlike : TransformationCondition
+   {
+      // Returns true if pawn is humanlike.
+      protected override bool CheckPartWorker(Pawn pawn, object cause)
+      {
+         return pawn.RaceProps.Humanlike;
+      }
+   }
+}
