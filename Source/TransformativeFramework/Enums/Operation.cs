07b91023413ace7aaf1a615c9c8736188c4f2000
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-12-08 11:13:04 -0500

    opps

--- Source/TransformativeFramework/Enums/Operation.cs
+++ Source/TransformativeFramework/Enums/Operation.cs
@@ -10,7 +10,7 @@ namespace LoonyLadle.TFs
 		None = 0,
 		Increase = 1,
 		Decrease = 2,
-		Normal = 3, TraitSpectrum = 7,
+		Normal = 3, TraitSpectrum = 3,
 		RemoveAtZero = 4,
 		Remove = 7, TraitSingular = 7,
 	}
