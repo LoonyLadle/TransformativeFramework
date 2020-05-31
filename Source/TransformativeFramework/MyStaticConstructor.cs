b19c29485b03e128a1d5f16be7b16cf1a763dcb6
LoonyLadle <LoonyLadle@users.noreply.github.com>
2020-05-31 11:47:07 -0400

    initial .NET/Harmony update
    Update to use .NET Framework 4.7.2 and Harmony 2.0.

--- Source/TransformativeFramework/MyStaticConstructor.cs
+++ Source/TransformativeFramework/MyStaticConstructor.cs
@@ -1,4 +1,4 @@
-﻿using Harmony;
+﻿using HarmonyLib;
 using Verse;
 
 #pragma warning disable IDE1006 // Naming Styles
@@ -10,7 +10,7 @@ namespace LoonyLadle.TFs
 	{
 		static MyStaticConstructor()
 		{
-			HarmonyInstance harmony = HarmonyInstance.Create("rimworld.loonyladle.tfs");
+			Harmony harmony = new Harmony("rimworld.loonyladle.tfs");
 			harmony.PatchAll();
 		}
 	}
--- Source/TransformativeFramework/Utilities/EnumUtility.cs
+++
@@ -1,9 +0,0 @@
-﻿#pragma warning disable IDE1006 // Naming Styles
-
-namespace LoonyLadle.TFs
-{
-	public static class EnumUtility
-	{
-		public static bool HasFlag(this Operation operation, Operation flag) => (operation & flag) == flag;
-	}
-}
